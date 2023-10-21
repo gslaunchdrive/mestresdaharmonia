7dfb2e29227b2365ef53f5e52914d37c14b64dbb
VGS <gs.launchdrive@gmail.com>
2023-10-21 17:44:41 -0300

    obrigado

---
+++ lc2org-obrigado/js/main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+
+    let groupWhats = 'https://devzapp.com.br/api-engennier/campanha/api/redirect/64ea7f79a29661000129421a';
+
+    function loadConfigs() {
+        // $.get( "https://glcdn.githack.com/ddmlaunch/configs/-/raw/dominandoacordes/obrigado.json",  function( data ) {
+        //     var config = "data[0]";
+        //     groupWhats = "https://devzapp.com.br/api-engennier/campanha/api/redirect/64ea7f79a29661000129421a";
+            
+            $('#whatsapp').attr("href", groupWhats);
+            initializeClock('clock', '2023-11-13T11:00:00Z');
+        // });
+    }
+
+    function redirectWhatsApp() {
+        setTimeout(function () {
+            // window.location.href = groupWhats;
+        }, 20000);
+    }
+    loadConfigs();
+    redirectWhatsApp();
+});
